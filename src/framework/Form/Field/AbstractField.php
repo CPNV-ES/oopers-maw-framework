e86fa8dbb977a056bb7ae3a7d41b521cee707c9e
dimitri-rutz <dimitri@drutz.ch>
2023-10-11 14:02:55 +0200

    feat: Add setter, getter and adder methods in AbstractField

--- src/framework/Form/Field/AbstractField.php
+++ src/framework/Form/Field/AbstractField.php
@@ -32,5 +32,71 @@ class AbstractField
 		$this->property = $property;
 	}
 
+	public function getId(): string
+	{
+		return $this->id;
+	}
+
+	public function setId(string $id): AbstractField
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	public function getValue(): mixed
+	{
+		return $this->value;
+	}
+
+	public function setValue(mixed $value): AbstractField
+	{
+		$this->value = $value;
+		return $this;
+	}
+
+	public function getOptions(): array
+	{
+		return $this->options;
+	}
+
+	public function setOptions(array $options): AbstractField
+	{
+		$this->options = $options;
+		return $this;
+	}
+
+	public function addOption(string $key, array|string $option): AbstractField
+	{
+		$this->options[$key] = $option;
+		return $this;
+	}
+
+	public function getError(): array
+	{
+		return $this->error;
+	}
+
+	public function hasError(): bool
+	{
+		return !empty($this->error);
+	}
+
+	public function setError(array $error): AbstractField
+	{
+		$this->error = $error;
+		return $this;
+	}
+
+	public function getProperty(): \ReflectionProperty
+	{
+		return $this->property;
+	}
+
+	public function setProperty(\ReflectionProperty $property): AbstractField
+	{
+		$this->property = $property;
+		return $this;
+	}
+
 
 }
