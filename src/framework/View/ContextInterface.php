94a596427cd29b9d1aa851f84b3789738b816660
dimitri-rutz <dimitri@drutz.ch>
2023-10-12 15:31:03 +0200

    feat: fill ContextInterface

--- src/framework/View/ContextInterface.php
+++ src/framework/View/ContextInterface.php
@@ -2,7 +2,31 @@
 
 namespace MVC\View;
 
+use MVC\Http\Request;
+
 interface ContextInterface
 {
+
+	/**
+	 * Used to resolve current request
+	 * @return Request
+	 */
+	public function getRequest(): Request;
+
+	/**
+	 * Generate url from path name
+	 * @param string $route
+	 * @param array $params
+	 * @return string
+	 */
+	public function path(string $route, array $params = []): string;
+
+	/**
+	 * Resolve item stored in context
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get(string $key): mixed;
+
 
 }
