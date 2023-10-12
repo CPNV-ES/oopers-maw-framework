ca6344951c0332913ccaa39c284ecdaf916b5417
dimitri-rutz <dimitri@drutz.ch>
2023-10-12 23:33:42 +0200

    feat: Implement context interface

---
+++ src/framework/View/Context.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MVC\View;
+
+use MVC\Http\Request;
+use MVC\Http\Routing\Exception\NotFoundRouteException;
+use MVC\Kernel;
+
+/**
+ * Object passed to vue to allow getting app context information
+ */
+class Context implements ContextInterface
+{
+
+	public Request $request;
+	private array $vars;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function url(string $route, array $params = []): string
+	{
+		try {
+			return Kernel::url($route, $params);
+		} catch (NotFoundRouteException $e) {
+			return "";
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function setVars(array $vars): Context
+	{
+		$this->vars = $vars;
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function add(array $vars): Context
+	{
+		$this->vars = $vars;
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __get(string $key)
+	{
+		if (!array_key_exists($key, $this->vars)) throw new ViewException("Unable to retrieve `$key` item in context view.");
+		return $this->vars[$key];
+	}
+}
