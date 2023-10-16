4f0816dafc9cba801d1ade0096ac425d617f63c9
dimitri-rutz <dimitri@drutz.ch>
2023-10-16 17:30:32 +0200

    feat: Add static method to build a form view

--- src/framework/Form/FormView.php
+++ src/framework/Form/FormView.php
@@ -7,4 +7,13 @@ use MVC\View\View;
 class FormView extends View
 {
 
+	public AbstractForm $form;
+
+	public static function createFromForm(AbstractForm $form): self
+	{
+		$view = new self($form->getOption('view_template'));
+		$view->form = $form;
+		return $view;
+	}
+
 }
