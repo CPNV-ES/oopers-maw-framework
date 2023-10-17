c27d693fc1c60c1c1a64a7d991ae2b99b7f0f303
dimitri-rutz <dimitri@drutz.ch>
2023-10-17 10:21:16 +0200

    feat: create utils for form render

--- src/framework/Form/FormView.php
+++ src/framework/Form/FormView.php
@@ -2,6 +2,7 @@
 
 namespace MVC\Form;
 
+use MVC\Form\Field\AbstractField;
 use MVC\View\View;
 
 class FormView extends View
@@ -13,7 +14,27 @@ class FormView extends View
 	{
 		$view = new self($form->getOption('view_template'));
 		$view->form = $form;
+		$view->context->add(['form' => $view]);
 		return $view;
+	}
+
+	public function field(string $name): AbstractField
+	{
+		return $this->form->getField($name);
+	}
+
+	public function start(array $attributes = []): string
+	{
+		return <<<HTML
+			<form action="{$this->form->getOption('action_route')}" {$this->form->getAttributes()} method="{$this->form->getOption('method')}">
+		HTML;
+	}
+
+	public function end(): string
+	{
+		return <<<HTML
+			</form>
+		HTML;
 	}
 
 }
