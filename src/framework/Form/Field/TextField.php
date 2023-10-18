f6f8f83424c2ee9bb15c6b66bd547fd3bda9742b
dimitri-rutz <dimitri@drutz.ch>
2023-10-18 10:30:53 +0200

    feat: respect signature of AbstractField

--- src/framework/Form/Field/TextField.php
+++ src/framework/Form/Field/TextField.php
@@ -5,9 +5,9 @@ namespace MVC\Form\Field;
 class TextField extends AbstractField
 {
 
-	public function __construct(string $id, mixed $value, \ReflectionProperty $property)
+	public function __construct(string $id, mixed $value, \ReflectionProperty $property, object $entity)
 	{
-		parent::__construct($id, $value, $property);
+		parent::__construct($id, $value, $property, $entity);
 		$this->setOption('attributes', ['class' => ['form-input']]);
 		$this->setOption('view_template', 'form.text-field');
 	}
