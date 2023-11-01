b4649eac622ee1a6ad80a377ed976006d4543176
dimitri-rutz <dimitri@drutz.ch>
2023-11-01 10:23:45 +0100

    feat: add option resolver class

---
+++ src/framework/OptionsResolver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MVC;
+
+class OptionsResolver
+{
+
+	/**
+	 * The default options
+	 * @var array
+	 */
+	private array $defaults = [];
+
+	/**
+	 * The required options
+	 * @var array
+	 */
+	private array $required = [];
+
+	/**
+	 * The allowed options
+	 * @var array
+	 */
+	private array $allowed = [];
+
+
+
+
+}
