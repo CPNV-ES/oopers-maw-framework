6215f6cc6f2690ff35b884f9ea42843ef1f38b88
dimitri-rutz <dimitri@drutz.ch>
2023-11-12 14:35:41 +0100

    feat: Add required and defaults values for Form Options

--- src/framework/Form/FormOptionResolverFactory.php
+++ src/framework/Form/FormOptionResolverFactory.php
@@ -17,6 +17,13 @@ class FormOptionResolverFactory
 				'attributes',
 				'method',
 			])
+			->setRequired([
+				'method',
+			])
+			->setDefaults([
+				'method' => 'POST',
+				'attributes' => []
+			])
 		;
 		
 	}
