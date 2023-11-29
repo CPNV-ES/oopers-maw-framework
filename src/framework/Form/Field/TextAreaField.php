6d0efc0f604d1ba40af2738a5a6730b6ef48daf9
dimitri-rutz <dimitri@drutz.ch>
2023-11-29 10:04:20 +0100

    feat: create text area filed

---
+++ src/framework/Form/Field/TextAreaField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MVC\Form\Field;
+
+class TextAreaField extends AbstractField
+{
+
+	public function __construct(string $id, mixed $value, \ReflectionProperty $property, object $entity)
+	{
+		parent::__construct($id, $value, $property, $entity);
+		$this->setOption('attributes', ['class' => ['form-input']]);
+		$this->setOption('view_template', 'form.text_area-field');
+	}
+}
