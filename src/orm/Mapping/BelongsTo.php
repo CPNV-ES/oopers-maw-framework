f15e50efb7b6d2e5bcbe197d600e9f68d616faff
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-06 10:18:35 +0100

    feat: create mapping for ManyToOne relations "child" side

---
+++ src/orm/Mapping/BelongsTo.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ORM\Mapping;
+
+use Attribute;
+use ReflectionProperty;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class BelongsTo
+{
+
+    public function __construct(
+        public string $inversedBy,
+        public ?string $entity = null
+    )
+    {
+    }
+
+    public function setEntityFromPropertyReflection(ReflectionProperty $property): self
+    {
+        if ($property->getType() instanceof \ReflectionUnionType) {
+            foreach ($property->getType()->getTypes() as $type) {
+                if (class_exists($type->getName())) $this->entity = $type->getName();
+            }
+        } else
+        {
+            $this->entity = $property->getType()->getName();
+        }
+        return $this;
+    }
+
+}
