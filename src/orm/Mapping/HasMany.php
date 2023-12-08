d168a66b62337587b5cd1b797ce2fd98db8d7b95
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-08 09:37:57 +0100

    feat: add targeted entity validation and instance creation of reflection for targeted property

--- src/orm/Mapping/HasMany.php
+++ src/orm/Mapping/HasMany.php
@@ -3,16 +3,21 @@
 namespace ORM\Mapping;
 
 use Attribute;
+use ORM\Exception\MappingException;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class HasMany
 {
 
+    private \ReflectionProperty $targetProperty;
+
     public function __construct(
-        public string $entity,
-        public string $targetProperty
+        private readonly string $entity,
+        string $targetProperty
     )
     {
+        if (!MappedEntity::isValidEntity($this->entity)) throw new MappingException(sprintf("Unable to find `%s` class for relational mapping.", $this->entity));
+        $this->targetProperty = new \ReflectionProperty($this->entity, $targetProperty);
     }
 
 }
