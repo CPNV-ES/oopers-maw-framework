b6a36e7725508a6f484cb6817208f59994ab4f07
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-13 08:37:56 +0100

    feat: add simple query utilities

--- src/orm/QueryBuilder.php
+++ src/orm/QueryBuilder.php
@@ -7,13 +7,44 @@ class QueryBuilder
 
     private array $aliases = [];
 
+    private array $fields = [];
+
     public function __construct(
-        private string $from,
+        private string $table,
         ?string $alias
     )
     {
-        $this->aliases[$this->from] = $alias;
-        $this->from .= " " . $alias;
+        $this->aliases[$this->table] = $alias;
+        $this->table .= " " . $alias;
+    }
+
+    public function select(array $fields): self
+    {
+        $this->fields = $fields;
+        return $this;
+    }
+
+    public function query(): string
+    {
+        return "SELECT {$this->getSelect()} FROM {$this->table}";
+    }
+
+    private function getFrom(): string
+    {
+        return $this->table . " " . $this->aliases[$this->table];
+    }
+
+    private function getSelect(): string
+    {
+        $fields = [];
+        foreach ($this->fields as $key => $field) {
+            if (is_string($key)) {
+                $fields[] = $field . " AS " . "'$key'";
+            } else {
+                $fields[] = $field;
+            }
+        }
+        return implode(', ', $fields);
     }
 
 }
