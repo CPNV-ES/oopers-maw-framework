88d5efe76b0a6ea78ff941ca5401087265ca5d99
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2023-12-15 08:25:26 +0100

    feat: create QueryBuilder test

---
+++ tests/ORM/QueryBuilderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\ORM;
+
+use ORM\QueryBuilder;
+use PHPUnit\Framework\TestCase;
+
+class QueryBuilderTest extends TestCase
+{
+
+    private function getQueryBuilder()
+    {
+        return new QueryBuilder();
+    }
+
+    public function testSimpleSelect()
+    {
+
+
+
+    }
+
+}
