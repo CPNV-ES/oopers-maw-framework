40b543589faac18ac13e36c9780a9d03bfe1b706
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-12-22 09:04:10 +0100

    feat : added possibility to get all as a map (id is key)

--- src/orm/EntitiesTraits/GetAll.php
+++ src/orm/EntitiesTraits/GetAll.php
@@ -18,4 +18,18 @@ trait GetAll
     {
         return $operations->fetchAll(self::class, $whereCondition);
     }
+
+    /**
+     * Get all entities instances as a map
+     * @param DatabaseOperations $operations - The db operations executor that will be used
+     * @param array $whereCondition - The where condition map key(column name) -> value
+     * @return array - All entities matching the whereCondition (everyone by default) as a map with the key = id
+     */
+    public static function getAllInMap(DatabaseOperations $operations, array $whereCondition = []) : array
+    {
+        return array_reduce(self::getAll($operations, $whereCondition), function($res, $entity) {
+            $res[$entity->getId()] = $entity;
+            return $res;
+        }, []);
+    }
 }
